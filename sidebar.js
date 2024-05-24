e4a2301411599bfe034e64082740cd72062205b3
eduardocamarena96 <eduardocamarenabvt2024@gmail.com>
2024-05-23 22:24:47 -0700

    First draft for desktop layout

--- sidebar.js
+++ sidebar.js
@@ -1,20 +1,46 @@
 const toggleBtn = document.querySelector(".hamburger-menu");
 const closeBtn = document.querySelector(".close-btn");
-const sidebar = document.querySelector(".navigation-sidebar");
+const sidebar = document.querySelector(".navigation-sliding-sidebar");
 
 toggleBtn.addEventListener("click", function () {
   sidebar.classList.toggle("show-sidebar");
-
-  // Select all elements
-  var allElements = document.querySelectorAll(".navigation-sidebar-option *");
-
-  // Loop through each element and change text color to white
-  allElements.forEach(function (element) {
-    element.style.color = "white";
-  });
-  
 });
 
 closeBtn.addEventListener("click", function () {
   sidebar.classList.remove("show-sidebar");
+});
+
+
+
+const navigationSidebar = document.getElementById('navigation-sidebar');
+const initialNavigationSidebarOption = document.getElementById('inbox-navigation-sidebar-option');
+const initialMessagesAmount = document.getElementById('inbox-messages-amount');
+
+let previousNavigationSidebarOption = null;
+let previousMessagesAmount = null;
+
+navigationSidebar.addEventListener('click', (event) => {
+  if (event.target && event.target.classList.contains('navigation-sidebar-option')) {
+      // Handle the click event on the child element
+
+      initialNavigationSidebarOption.classList.remove('navigation-sidebar-option--highlight');
+      initialMessagesAmount.classList.remove('messages-amount--highlight');
+
+      if (previousNavigationSidebarOption) {
+        previousNavigationSidebarOption.classList.remove('navigation-sidebar-option--highlight');
+      }
+
+      if (previousMessagesAmount) {
+        previousMessagesAmount.classList.remove('messages-amount--highlight');
+      }
+
+      const navigationSidebarOption = event.target;
+      const messagesAmount = navigationSidebarOption.querySelector('.messages-amount');
+
+      navigationSidebarOption.classList.add('navigation-sidebar-option--highlight');
+      messagesAmount.classList.add('messages-amount--highlight');
+
+      previousNavigationSidebarOption = navigationSidebarOption;
+      previousMessagesAmount = messagesAmount;
+  }
 });
